a5f3ee5daeebaf5d998615db927c412c752d948f
hthomp15 <hailethomp@gmail.com>
2022-05-17 23:09:07 -0400

    add colors

--- petbnb/src/App.js
+++ petbnb/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PetBnB</h1>
+        <p> Welcome to PetBnB!</p>
+        <img src={require('./assets/images/stock-photos/pets-0.jpeg')} alt="cats" />
+        <span> lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</span> 
       </header>
     </div>
   );
