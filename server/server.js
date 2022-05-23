879292016da329b6ada8ecf8cc7e823d0c42c16b
Mohammad Rahman <mbrahman@gmail.com>
2022-05-23 15:52:04 -0400

    added graphql and mongodb atlas connection

--- server/server.js
+++ server/server.js
@@ -1,2 +1,30 @@
 const express = require('express')
+const db = require('./config/connection')
 const { ApolloServer } = require('apollo-server-express')
+const { typeDefs, resolvers } = require('./schemas')
+
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start()
+    server.applyMiddleware({ app })
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}`)
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+        })
+    })
+}
+
+startApolloServer(typeDefs, resolvers)
