ca2a7a05a839826e62baba8afa814df9b1b7f671
Alston Pough <alstonpough@Alstons-MacBook-Pro.local>
2022-05-24 15:08:08 -0400

    dashboard structure

--- petbnb/src/App.js
+++ petbnb/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header, Navbar } from './components/Common'
+import Dashboard from './components/Dashboard/dashboard';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-
+      <Dashboard />
     </div>
   );
 }
