ae3492e58f23a2334eb133999a9bc7cf01fd28a0
hthomp15 <hailethomp@gmail.com>
2022-05-26 15:09:42 -0400

    add dashboard content

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USERS = gql`
+export const QUERY_USER_POST = gql`
 query Users {
   users {
     _id
@@ -16,3 +16,41 @@ query Users {
   }
 }
 `;
+
+export const QUERY_PETS = gql`
+query Pets {
+  pets {
+    _id
+    petName
+    petType
+    petNotes
+  }
+}
+`;
+
+export const QUERY_USERS = gql`
+query Query {
+  users {
+    _id
+    username
+    email
+    password
+  }
+}
+`;
+
+export const QUERY_POSTS = gql`
+query Query {
+  posts {
+    _id
+    postText
+    createdAt
+    username
+    comments {
+      _id
+      commentText
+      createdAt
+      username
+    }
+  }
+}`
