552a1cff5609427cd4b3188e49166232c32a3101
Mohammad Rahman <mbrahman@gmail.com>
2022-06-02 17:08:50 -0400

    final push at 5:08

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,9 @@
-import { useState, useEffect }from 'react';
-import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 
 function Header() {
-  const logout = event => {
+  const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
@@ -12,14 +12,14 @@ function Header() {
     <header className="header">
       <div className="header-content">
         <div className="header-content-left">
-            <Link to="/">
-                <h1>PetBnB</h1>
-            </Link>
+          <Link to="/">
+            <h1>PetBnB</h1>
+          </Link>
         </div>
         <nav className="header-content-right">
-            <Link to="/about"> About Us</Link>
+          <Link to="/about"> About Us</Link>
 
-            {Auth.loggedIn() ? (
+          {Auth.loggedIn() ? (
             <>
               <Link to={`/profile/`}>Me</Link>
               <a href="/" onClick={logout}>
@@ -32,14 +32,13 @@ function Header() {
               <Link to="/signup">Signup</Link>
             </>
           )}
-            {Auth.loggedIn() ? (
-              <>
+          {Auth.loggedIn() ? (
+            <>
               <Link to="/newpost"> New Post </Link>
-              </>
-              ) : (
-                <>
-                </>
-                )}
+            </>
+          ) : (
+            <></>
+          )}
         </nav>
       </div>
     </header>
--- client/src/pages/NewPost.js
+++ client/src/pages/NewPost.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import { ADD_POSTS} from '../utils/mutations';
-import { useMutation } from '@apollo/client';
-
+import React, { useState } from "react";
+import { ADD_POSTS } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 
 const NewPost = () => {
-  const[newPost, { error }] = useMutation(ADD_POSTS);
+  const [newPost, { error }] = useMutation(ADD_POSTS);
   const [input, setInput] = useState({
-    username: '',
-    dateNeeded: '',
-    postText: ''
+    // username: "",
+    dateNeeded: "",
+    postText: "",
   });
 
   async function submitForm(e) {
     e.preventDefault();
-    console.log('input', input);
+    console.log("input", input);
     try {
-      const { data } = await newPost( {
-        variables: { ...input },
+      const { data } = await newPost({
+        variables: { input: { ...input } },
       });
-      console.log('data', data);
+      console.log("data", data);
     } catch (e) {
       console.error(e);
     }
@@ -32,29 +31,43 @@ const NewPost = () => {
   }
 
   return (
-    <form onSubmit={(e)=> submitForm(e)} >
-        <div>
-            <p>Username</p>
-            <input onChange={(e)=> handleChange(e)} type="text" name="username" value={input.username}/>
-            <label></label>
-        </div>
-        <div>
-            <p>Dates Needed:</p>
-            <input onChange={(e)=> handleChange(e)} type="text" name="dateNeeded" value={input.dateNeeded}/>
-            <label></label>
-        </div>
-        <div>
-            <p>Post Text</p>
+    <form onSubmit={(e) => submitForm(e)}>
+      {/* <div>
+        <p>Username</p>
+        <input
+          onChange={(e) => handleChange(e)}
+          type="text"
+          name="username"
+          value={input.username}
+        />
+        <label></label>
+      </div> */}
+      <div>
+        <p>Dates Needed:</p>
+        <input
+          onChange={(e) => handleChange(e)}
+          type="text"
+          name="dateNeeded"
+          value={input.dateNeeded}
+        />
+        <label></label>
+      </div>
+      <div>
+        <p>Post Text</p>
 
-            <input onChange={(e)=> handleChange(e)} type="text" name="postText" value={input.postText}/>
-            <label></label>
-        </div>
-        <button onClick={(e)=>submitForm(e)}type="submit">Submit</button>
-
+        <input
+          onChange={(e) => handleChange(e)}
+          type="text"
+          name="postText"
+          value={input.postText}
+        />
+        <label></label>
+      </div>
+      <button onClick={(e) => submitForm(e)} type="submit">
+        Submit
+      </button>
     </form>
   );
 };
 
 export default NewPost;
-
-
