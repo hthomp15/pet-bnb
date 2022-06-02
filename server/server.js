f11e84f0b9a47603a7cf34542d3dfaa15e884d06
hthomp15 <hailethomp@gmail.com>
2022-06-01 20:01:23 -0400

    server update

--- server/server.js
+++ server/server.js
@@ -1,41 +1,40 @@
-const express = require('express')
-const path = require('path')
-const db = require('./config/connection')
-const { ApolloServer } = require('apollo-server-express')
-const { typeDefs, resolvers } = require('./schemas')
-const { authMiddleware } = require('./utils/auth')
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
 
-const PORT = process.env.PORT || 4000
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
-})
-
-const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start()
-    server.applyMiddleware({ app })
-
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
-      }
-      
-      app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-        });
-        
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}`)
-            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
-        })
-    })
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-startApolloServer(typeDefs, resolvers)
+// app.use(routes);
+
+// create a new Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// create a new instance of Apollo server
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
+startApolloServer(typeDefs, resolvers);
