f7c793ba9f31ecb05fb87bc5764b3c911ada14e1
Mohammad Rahman <mbrahman@gmail.com>
2022-06-02 16:34:51 -0400

    fixed add post bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,151 +1,171 @@
-const { User, Pet, Post } = require('../models')
-const { AuthenticationError } = require('apollo-server-express')
-const { signToken } = require('../utils/auth')
+const { User, Pet, Post } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find()
-                .populate('posts')
-                .populate('pets')
-        },
-        user: async (parent, { username }) => {
-            return User.findOne({ username})
-                .populate('posts')
-                .populate('pets')
-        },
-        pets: async () => {
-            return Pet.find()
-        },
-        pet: async (parent, { _id }) => {
-            return Pet.findOne({ _id })
-        },
-        posts: async () => {
-            return Post.find()
-        },
-        post: async (parent, { _id }) => {
-            return Post.findOne({ _id })
-        },
-        me: async (parent, args, context) => {
-            if (context.user) {
-              const userData = await User.findOne({ _id: context.user._id })
-                .select('-__v -password')
-                .populate('posts')
-                .populate('pets');
-          
-              return userData;
-            }
-          
-            throw new AuthenticationError('Not logged in');
-          }
-          
-
+  Query: {
+    users: async () => {
+      return User.find().populate("posts").populate("pets");
     },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("posts").populate("pets");
+    },
+    pets: async () => {
+      return Pet.find();
+    },
+    pet: async (parent, { _id }) => {
+      return Pet.findOne({ _id });
+    },
+    posts: async () => {
+      return Post.find();
+    },
+    post: async (parent, { _id }) => {
+      return Post.findOne({ _id });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("posts")
+          .populate("pets");
 
-    Mutation: {
-        addUser: async (parent, { email, password, phone, username }) => {
-            console.log('REDDDDDD', username, email, password, phone)
-            const user = await User.create({ email, password, phone, username })
-            // user will return _id, username, email
-            const token = signToken(user)
+        return userData;
+      }
 
-            return { user, token }
-        },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email })
+      throw new AuthenticationError("Not logged in");
+    },
+  },
 
-            // check if email exists
-            if (!user) {
-                throw new AuthenticationError('Incorrect email address')
-            }
+  Mutation: {
+    addUser: async (parent, { email, password, phone, username }) => {
+      console.log("REDDDDDD", username, email, password, phone);
+      const user = await User.create({ email, password, phone, username });
+      // user will return _id, username, email
+      const token = signToken(user);
 
-            // check if password matches
-            const correctPw = await user.isCorrectPassword(password)
+      return { user, token };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect password')
-            }
+      // check if email exists
+      if (!user) {
+        throw new AuthenticationError("Incorrect email address");
+      }
 
-            const token = signToken(user)
-            return { user, token }
-        },
-        deleteUser: async (parent, args) => {
-            const user = await User.findOneAndDelete({ _id: args._id })
+      // check if password matches
+      const correctPw = await user.isCorrectPassword(password);
 
-            return user
-        },
-        updateUser: async (parent, args) => {
-            const user = await User.findByIdAndUpdate(args._id, args.input, { new: true })
-            return user
-        },
-        addPet: async (parent, args, context) => {
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password");
+      }
 
-            console.log(context.user)
+      const token = signToken(user);
+      return { user, token };
+    },
+    deleteUser: async (parent, args) => {
+      const user = await User.findOneAndDelete({ _id: args._id });
 
-            if (context.user) {
-                console.log('HELLOOOOO')
-                const pet = await Pet.create({ ...args.input, username: context.user.username })
-                await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $push: { pets: pet._id } },
-                    { new: true }
-                )
-                return pet
-            }
+      return user;
+    },
+    updateUser: async (parent, args) => {
+      const user = await User.findByIdAndUpdate(args._id, args.input, {
+        new: true,
+      });
+      return user;
+    },
+    addPet: async (parent, args, context) => {
+      console.log(context.user);
 
-            throw new AuthenticationError('You need to be logged in!')
-        },
-        deletePet: async (parent, args) => {
-            const pet = await Pet.findOneAndDelete({ _id: args._id })
+      if (context.user) {
+        console.log("HELLOOOOO");
+        const pet = await Pet.create({
+          ...args.input,
+          username: context.user.username,
+        });
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { pets: pet._id } },
+          { new: true }
+        );
+        return pet;
+      }
 
-            return pet
-        },
-        updatePet: async (parent, args) => {
-            const pet = await Pet.findByIdAndUpdate(args._id, args.input, { new: true })
-            return pet
-        },
-        addPost: async (parent, args, context) => {
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deletePet: async (parent, args) => {
+      const pet = await Pet.findOneAndDelete({ _id: args._id });
 
-            console.log(context.user)
+      return pet;
+    },
+    updatePet: async (parent, args) => {
+      const pet = await Pet.findByIdAndUpdate(args._id, args.input, {
+        new: true,
+      });
+      return pet;
+    },
+    addPost: async (parent, args, context) => {
+      console.log("HELLLLLLLLOOOOOOOOOO");
+      console.log(context.user);
 
-            if (context.user) {
-                const post = await Post.create({ ...args.input, username: context.user.username })
+      if (context.user) {
+      const post = await Post.create({ ...args.input, username: context.user.username })
 
-                await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $push: { posts: post._id } },
-                    { new: true }
-                )
-                return post
-            }
+      await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { posts: post._id } },
+          { new: true }
+      )
+      return post
+      }
+    //   const post = await Post.create({
+    //     ...args.input,
+    //   });
 
-            throw new AuthenticationError('You need to be logged in!')
-        },
-        deletePost: async (parent, args) => {
-            const post = await Post.findOneAndDelete({ _id: args._id })
+    //   console.log('POST', post)
 
-            return post
-        },
-        updatePost: async (parent, args) => {
-            const post = await Post.findByIdAndUpdate(args._id, args.input, { new: true })
-            return post
-        },
-        addComment: async (parent, args, context) => {
-            console.log(context.user)
-            console.log(context.user.username, 'HELLLOOOOO')
+    //   await User.findByIdAndUpdate(
+    //     { _id: context.user._id },
+    //     { $push: { posts: post._id } },
+    //     { new: true }
+    //   );
+    //   return post;
 
-            if (context.user) {
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deletePost: async (parent, args) => {
+      const post = await Post.findOneAndDelete({ _id: args._id });
 
-                const addedComment = await Post.findOneAndUpdate(
-                    { _id: args.postId },
-                    { $push: { comments: { username: context.user.username, commentText: args.commentText } } },
-                    { new: true }
-                )
-                return addedComment
-            }
-            throw new AuthenticationError('You need to be logged in!')
-        }
-    }
-}
+      return post;
+    },
+    updatePost: async (parent, args) => {
+      const post = await Post.findByIdAndUpdate(args._id, args.input, {
+        new: true,
+      });
+      return post;
+    },
+    addComment: async (parent, args, context) => {
+      console.log(context.user);
+      console.log(context.user.username, "HELLLOOOOO");
 
-module.exports = resolvers
+      if (context.user) {
+        const addedComment = await Post.findOneAndUpdate(
+          { _id: args.postId },
+          {
+            $push: {
+              comments: {
+                username: context.user.username,
+                commentText: args.commentText,
+              },
+            },
+          },
+          { new: true }
+        );
+        return addedComment;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,104 +1,113 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    phone: String
+    posts: [Post]
+    pets: [Pet]
+  }
 
-    type User {
-        _id: ID
-        username: String
-        email: String
-        password: String
-        phone: String
-        posts: [Post]
-        pets: [Pet]
-    }
+  type Post {
+    _id: ID
+    postText: String
+    commentCount: Int
+    dateNeeded: String
+    createdAt: String
+    username: String
+    comments: [Comment]
+  }
+  # comments are subdocuments of posts
+  type Comment {
+    _id: ID
+    commentText: String
+    createdAt: String
+    username: String
+  }
+  type Pet {
+    _id: ID
+    petName: String
+    petType: String
+    petNotes: String
+    username: String
+  }
 
-    type Post {
-        _id: ID
-        postText: String
-        commentCount: Int
-        dateNeeded: String
-        createdAt: String
-        username: String
-        comments: [Comment]
-    }
-    # comments are subdocuments of posts
-    type Comment {
-        _id: ID
-        commentText: String
-        createdAt: String
-        username: String
-    }
-    type Pet {
-        _id: ID
-        petName: String
-        petType: String
-        petNotes: String
-        username: String
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  # create common input fields for mutations
+  input UserInput {
+    username: String
+    email: String
+    password: String
+    phone: String
+  }
 
-    # create common input fields for mutations
-    input UserInput {
-        username: String
-        email: String
-        password: String
-        phone: String
-    }
+  # input PetInput {
+  #     petType: String
+  #     petNotes: String
+  #     petName: String
+  # }
 
-    input PetInput {
-        petType: String
-        petNotes: String
-        petName: String
-    }
+  input PetInput {
+    petType: String
+    petNotes: String
+    petName: String
+  }
 
-    input PostInput {
-        postText: String
-        username: String  
-        dateNeeded: String
-    }
+  input PostInput {
+    dateNeeded: String
+    postText: String
+    # username: String
+  }
 
-    input CommentInput {
-        commentText: String
-        username: String
-    }
+  input CommentInput {
+    commentText: String
+    username: String
+  }
 
-    type Query {
-        me: User
+  type Query {
+    me: User
 
-        users: [User]
-        user(username: String!): User
+    users: [User]
+    user(username: String!): User
 
-        pets: [Pet]
-        pet(_id: ID!): Pet
+    pets: [Pet]
+    pet(_id: ID!): Pet
 
-        posts: [Post]
-        post(_id: ID!): Post
+    posts: [Post]
+    post(_id: ID!): Post
 
-        comments: [Comment]
-        comment(_id: ID!): Comment
-    }
+    comments: [Comment]
+    comment(_id: ID!): Comment
+  }
 
-    type Mutation {
-        # addUser(input: UserInput!): Auth
-        addUser(username: String!, email: String!, password: String!, phone: String!): Auth
-        login(email: String!, password: String!): Auth
-        deleteUser(_id: ID!): User
-        updateUser(_id: String!, input: UserInput!): User
+  type Mutation {
+    # addUser(input: UserInput!): Auth
+    addUser(
+      username: String!
+      email: String!
+      password: String!
+      phone: String!
+    ): Auth
+    login(email: String!, password: String!): Auth
+    deleteUser(_id: ID!): User
+    updateUser(_id: String!, input: UserInput!): User
 
-        addPet(input: PetInput!): Pet
-        deletePet(_id: ID!): Pet
-        updatePet(_id: String!, input: PetInput!): Pet
+    addPet(input: PetInput!): Pet
+    deletePet(_id: ID!): Pet
+    updatePet(_id: String!, input: PetInput!): Pet
 
-        addPost(input: PostInput!): Post
-        deletePost(_id: ID!): Post
-        updatePost(_id: String!, input: PostInput!): Post
-        addComment(postId: ID!, commentText: String!): Post
-    }
+    addPost(input: PostInput!): Post
+    deletePost(_id: ID!): Post
+    updatePost(_id: String!, input: PostInput!): Post
+    addComment(postId: ID!, commentText: String!): Post
+  }
+`;
 
-`
-
-module.exports = typeDefs
+module.exports = typeDefs;
